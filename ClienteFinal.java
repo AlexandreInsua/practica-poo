0916b93ffa662606b3644a5591a55b8709ccd34d
Alexandre Insua <alexandreinsua@gmail.com>
2023-04-08 21:12:59 +0200

    feat: add first draft of arquitecture

---
+++ ClienteFinal.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class ClienteFinal here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClienteFinal extends Cliente
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class ClienteFinal
+     */
+    public ClienteFinal()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ Grande.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Grande here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Grande extends Productor
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Grande
+     */
+    public Grande()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
