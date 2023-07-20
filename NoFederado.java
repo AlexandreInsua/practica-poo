775d5759cc4c6301d7268e28f0c19c2ed7b6fac3
Alexandre Insua <alexandreinsua@gmail.com>
2023-07-20 17:15:47 +0200

    chore(projec): add documentation comment

--- NoFederado.java
+++ NoFederado.java
@@ -1,17 +1,34 @@
+import java.util.List;
 
 /**
- * Write a description of class NoFederado here.
+ * Clase abstracta NoFederado representa una abstracción que modela 
+ * el comportamiento común a las clases GranProductor y PeqProductor.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Alexandre Insua Moreira
+ * @version 
  */
 public abstract class NoFederado extends Productor
 {
-
+    /**
+     * Constructor de la clase NoFederado.
+     * @param nombre El nombre del productor no federado.
+     */
     public NoFederado(String nombre)
     {
         super(nombre);
     }
 
-    public abstract boolean asignarProducto(ProductoProductor producto);       
+    /**
+     * Método abstracto para asignar un producto al productor no federado.
+     * @param producto El producto a ser asignado al productor no federado.
+     * @return true si el producto se asignó correctamente, false en caso contrario.
+     */
+    public abstract boolean asignarProducto(ProductoProductor producto); 
+
+    /**
+     * Método abstracto para obtener la lista de productos del productor no federado.
+     *
+     * @return Una lista de objetos ProductoProductor que representa los productos asignados al productor no federado.
+     */
+    public abstract List<ProductoProductor> getProductos();
 }
